ca13467b4bf3934c7c97425c29dec2487d3db1f2
kaz <kazan19066@gmail.com>
2024-11-22 19:22:48 +0400

    changed implementations

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-buildscript {
+    buildscript {
 
     repositories {
         google()
@@ -12,9 +12,9 @@ buildscript {
 
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
-        classpath("com.google.gms:google-services:4.4.2")
+        classpath("com.google.gms:google-services:4.3.15")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.48")
-        classpath ("com.android.tools.build:gradle:8.2.2")
+        classpath ("com.android.tools.build:gradle:8.1.0")
 
     }
 
